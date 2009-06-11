0821159d6d9ee32b5ae87a70bc6c6134df5978fe
Johan Ronsse <johan.ronsse@gmail.com>
2009-06-11 19:09:50 +0200

    Added buttons test, refined buttons CSS

---
+++ index.php
@@ -0,0 +1,7 @@
+<h1>Winston</h1>
+<ul>
+	<li><a href="docs">Documentation</a></li>
+	<li><a href="start_project/">Project start</a></li>
+	<li><a href="test_suite/buttons">Buttons test suite</a></li>
+	<li><a href="test_suite/layout">Layout test suite</a></li>
+</ul>
