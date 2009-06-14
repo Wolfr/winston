95ac974c6d0447088299811953066e993570d7dd
Johan Ronsse <johan.ronsse@gmail.com>
2009-06-14 17:40:17 +0200

    Moved index css to separate file

--- index.php
+++ index.php
@@ -3,63 +3,18 @@
 <html lang="en">
 <head>
 	<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-	
+
 	<title>Winston UI Library</title>
 
 	<!-- Winston -->
 	<link rel="stylesheet" href="start_project/css/winston.full.0.5.css" type="text/css" media="screen">
 
+	<!-- Index page CSS -->
+	<link rel="stylesheet" href="index.css" type="text/css" media="screen">
+
 	<!-- jQuery -->
 	<script src="start_project/js/jquery/jquery-1.3.2.min.js" type="text/javascript"></script>
-	
-	<style type="text/css" media="screen">
-		
-		h1 {
-			background: url(docs/images/winston_logo.png) no-repeat scroll top left;
-			width: 418px;
-			height: 97px;
-			font-size: 97px;
-			letter-spacing: -5px;
-			text-transform: uppercase;
-			line-height: 1;
-			text-indent: -9000px;
-			margin: 24px;
-		}
-		
-		li {
-			font-size: 18px;
-		}
-		
-		#resources {
-			width: 466px;
-			padding: 48px;
-			margin: 48px auto;
-			background: #FFF;
-			border: 1px dotted #CCC;
-			
-			-moz-border-radius: 6px;
-			-webkit-border-radius: 6px;
-			border-radius: 6px;
-		}
-		
-		#resources ul {
-			-moz-column-count: 2;
-			-webkit-column-count: 2;
-			column-count: 2;
-		}
-		
-		#resources li {
-			border-bottom: 1px solid #CCC;
-			padding: 0 0 6px;
-			margin: 0 0 6px;
-		}
-		
-		#resources li:last-child {
-			border: 0; margin: 0; padding: 0;
-		}
-		
-	</style>
-	
+
 </head>
 <body>
 
