089396ffbfa007d42089610d9f61d4b09fffc6ef
Johan Ronsse <johan.ronsse@gmail.com>
2009-06-14 17:00:02 +0200

    DONE * Link root index.php to relevant parts / Preparing 0.4 release

--- index.php
+++ index.php
@@ -1,7 +1,81 @@
-<h1>Winston</h1>
-<ul>
-	<li><a href="docs">Documentation</a></li>
-	<li><a href="start_project/">Project start</a></li>
-	<li><a href="test_suite/buttons">Buttons test suite</a></li>
-	<li><a href="test_suite/layout">Layout test suite</a></li>
-</ul>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+	"http://www.w3.org/TR/html4/strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+	
+	<title>Winston UI Library</title>
+
+	<!-- Winston -->
+	<link rel="stylesheet" href="start_project/css/winston.full.0.4.css" type="text/css" media="screen" />
+
+	<!-- jQuery -->
+	<script src="start_project/js/jquery/jquery-1.3.2.min.js" type="text/javascript"></script>
+	
+	<style type="text/css" media="screen">
+		
+		h1 {
+			background: url(docs/images/winston_logo.png) no-repeat scroll top left;
+			width: 418px;
+			height: 97px;
+			font-size: 97px;
+			letter-spacing: -5px;
+			text-transform: uppercase;
+			line-height: 1;
+			text-indent: -9000px;
+			margin: 24px;
+		}
+		
+		li {
+			font-size: 18px;
+		}
+		
+		#resources {
+			width: 466px;
+			padding: 48px;
+			margin: 48px auto;
+			background: #FFF;
+			border: 1px dotted #CCC;
+			
+			-moz-border-radius: 6px;
+			-webkit-border-radius: 6px;
+			border-radius: 6px;
+		}
+		
+		#resources ul {
+			-moz-column-count: 2;
+			-webkit-column-count: 2;
+			column-count: 2;
+		}
+		
+		#resources li {
+			border-bottom: 1px solid #CCC;
+			padding: 0 0 6px;
+			margin: 0 0 6px;
+		}
+		
+		#resources li:last-child {
+			border: 0; margin: 0; padding: 0;
+		}
+		
+	</style>
+	
+</head>
+<body>
+
+	<div id="resources">
+		<h1>Winston</h1>
+		<ul>
+			<li><a href="docs">Documentation</a></li>
+			<li><a href="start_project/">Project start</a></li>
+			<li><a href="test_suite/buttons">Buttons test suite</a></li>
+			<li><a href="test_suite/layout">Layout test suite</a></li>
+			<li><a href="test_suite/links">Links test suite</a></li>
+			<li><a href="test_suite/typography">Typography test suite</a></li>
+			<li><strong><a href="http://github.com/Wolfr/winston/tree/master">Winston on Github</a></strong></li>
+		</ul>
+	</div>
+
+</body>
+</html>
+
